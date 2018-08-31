64aa77b47643b5060fc0cec972497e3bb5c302e4
Ramadani <email.ramadani@gmail.com>
2018-08-31 14:40:10 +0700

    show tables

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const sqlTables = require('./sql.json');
+
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' });
+app.get('/', (_, res) => {
+  res.render('index', { tables: sqlTables });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
